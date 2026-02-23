4a7c18038e519ee386391202c44fe1073756700d
Rohith-AIDS <cchin2429@gmail.com>
2026-02-23 23:01:10 +0530

    Implement JwtUtil for generating, validating, and extracting JWT tokens

---
+++ src/main/java/com/project1/demo/security/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.project1.demo.security;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+	private final String SECRET="mysecretkeymysecretkeymysecretkey";
+	
+	public String generateToken(String username)
+	{
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis()+1000*60*60))
+				.signWith(Keys.hmacShaKeyFor(SECRET.getBytes()),SignatureAlgorithm.HS256)
+				.compact();
+	}
+	
+	public String extractUserName(String token)
+	{
+		return Jwts.parserBuilder()
+				.setSigningKey(SECRET.getBytes())
+				.build()
+				.parseClaimsJws(token)
+				.getBody()
+				.getSubject();
+	}
+	
+	public boolean validateToken(String token)
+	{
+		try {
+			Jwts.parserBuilder()
+			.setSigningKey(SECRET.getBytes())
+			.build()
+			.parseClaimsJws(token);
+			return true;
+		}
+		
+		catch(Exception e)
+		{
+			return false;
+		}
+	}
+}
